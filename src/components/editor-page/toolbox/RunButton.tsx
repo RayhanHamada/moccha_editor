57f3059e62adff392f59132ac6f680c6bca1cf53
RayhanHamada <hamadadesk1999@gmail.com>
2020-02-19 19:32:14 +0700

    add react terminal and styling run button

--- src/components/editor-page/toolbox/RunButton.tsx
+++ src/components/editor-page/toolbox/RunButton.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
+import { MyTypes } from '../../../store/app-custom-types';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-const RunButton = styled.button``;
+const Button = styled.button`
+	/* &::after */
+	&:hover {
+		background-color: #333333;
+		color: #e41749;
+	}
+`;
 
-RunButton.defaultProps = {
+Button.defaultProps = {
 	className:
 		'run-code mx-10 my-2 border border-black p-1 rounded flex flex-row items-center',
 	children: (
 		<>
-			<i className="fa fa-play-circle-o fa-2x"></i>
-			<span className="ml-1">Run Code</span>
+			<i className="fa fa-play-circle-o fa-2x" id="run-icon"></i>
+			<span className="ml-1" id="run-text">
+				Run Code
+			</span>
 		</>
 	),
 };
 
-export default RunButton;
+const mapStateToProps = ({}: MyTypes.RootState) => ({});
+
+const mapDispatchToProps = (dispatch: MyTypes.AppDispatch) =>
+	bindActionCreators({}, dispatch);
+
+type RunButtonProps = ReturnType<typeof mapStateToProps> &
+	ReturnType<typeof mapDispatchToProps>;
+
+const RunButton = (props: RunButtonProps) => {
+	return <Button />;
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RunButton);
