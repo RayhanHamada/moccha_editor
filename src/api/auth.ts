5fefb06eef0308bb55294fa6cf88609a50f47afa
RayhanHamada <hamadadesk1999@gmail.com>
2020-04-11 21:28:12 +0700

    change auth API version

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,11 +2,16 @@ import axios from 'axios';
 
 import { serverUrl as BASE_SERVER_URL } from '../globals';
 
-const GET_AUTH_ENDPOINT = `${BASE_SERVER_URL}/api/auth`;
-const GET_ROOM_KEY = `${BASE_SERVER_URL}/api/auth/getRoomKey`;
-const CHECK_ROOM_EXISTENCE = `${BASE_SERVER_URL}/api/auth/checkRoomExistence`;
-const GET_ROOM_KEYS = `${BASE_SERVER_URL}/api/auth/getRoomKeys`;
-const DELETE_ROOM_KEY = `${BASE_SERVER_URL}/api/auth/deleteRoomKey`;
+/*
+ * auth api version used
+ */
+const apiVersion = 'v1';
+
+const GET_AUTH_ENDPOINT = `${BASE_SERVER_URL}/api/auth/${apiVersion}`;
+const GET_ROOM_KEY = `${BASE_SERVER_URL}/api/auth/${apiVersion}/getRoomKey`;
+const CHECK_ROOM_EXISTENCE = `${BASE_SERVER_URL}/api/auth/${apiVersion}/checkRoomExistence`;
+const GET_ROOM_KEYS = `${BASE_SERVER_URL}/api/auth/${apiVersion}/getRoomKeys`;
+const DELETE_ROOM_KEY = `${BASE_SERVER_URL}/api/auth/${apiVersion}/deleteRoomKey`;
 
 export const getAuthEndpoint = () =>
 	axios.get(GET_AUTH_ENDPOINT).then(res => res.data as string);
