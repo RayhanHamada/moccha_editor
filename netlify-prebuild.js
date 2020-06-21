01d328805a824510098043239f2d5270a8473e5a
RayhanHamada <hamadadesk1999@gmail.com>
2020-06-21 12:59:25 +0700

    add envObj key prod

--- netlify-prebuild.js
+++ netlify-prebuild.js
@@ -3,10 +3,12 @@ const fs = require('fs');
 (async () => {
 	const envs = ['NODE_ENV', 'BASE_SERVER_URL', 'MOCK_BASE_SERVER_URL'];
 
-	const envObj = envs.reduce((prev, curr) => ({
-		...prev,
-		[curr]: JSON.stringify(process.env[curr]),
-	}));
+	const envObj = {
+		prod: envs.reduce((prev, curr) => ({
+			...prev,
+			[curr]: JSON.stringify(process.env[curr]),
+		})),
+	};
 
 	await fs.promises.writeFile('./.env-cmdrc.json', JSON.stringify(envObj));
 })();
